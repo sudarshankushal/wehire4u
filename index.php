fd31e3d29baa0781654e4c8c7c73e185b49e0e45
sudarshan subramanian <sudarshankushal@gmail.com>
2013-06-09 22:01:16 +0530

    git commit -m "first commit"

---
+++ index.php
@@ -0,0 +1,323 @@
+
+<!DOCTYPE html>
+
+
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Plivo Cloud | 
+Voice & SMS API Platform
+</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="https://plivo-static.s3.amazonaws.com/images/favicon.ico" />
+    <link href='https://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://plivo-static.s3.amazonaws.com/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://plivo-static.s3.amazonaws.com/css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="https://plivo-static.s3.amazonaws.com/css/styles.css">
+    <link rel="stylesheet" href="https://plivo-static.s3.amazonaws.com/css/mobile.css" type="text/css" media="screen and (max-width: 480px)" />
+    <link rel="stylesheet" href="https://plivo-static.s3.amazonaws.com/css/tablet.css" type="text/css" media="screen and (max-width: 768px)" />
+    
+
+    
+    <link rel="alternate" type="application/rss+xml" title="RSS" href="/blog/feeds/rss/">
+    <link rel="alternate" type="application/atom+xml" title="Atom" href="/blog/feeds/atom/">
+    
+    
+        <!--[if lt IE 9]>
+            <script src="https://plivo-static.s3.amazonaws.com/js/html5shiv.js" type="text/javascript"></script>
+        <![endif]-->
+
+    
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="navbar navbar-fixed-top navbar-shadow">
+          <div class="navbar-inner">
+            <div class="container">
+              <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </a>
+              <a class="brand" href="/">
+        <img src="https://plivo-static.s3.amazonaws.com/images/logo.png" class="logo" alt="Plivo Logo" />
+          </a>
+              <div class="nav-collapse collapse">
+                <ul class="nav">
+          
+                  <li><a href="/pricing/">Pricing</a></li>
+                  <li><a href="/docs/">Docs</a></li>
+          <li class="divider-vertical"></li>
+          <li class="header_contact">
+            <p><img src="https://plivo-static.s3.amazonaws.com/images/call_icon.png" alt="Call" /> +1 855-46-75486</p>
+            <p><img src="https://plivo-static.s3.amazonaws.com/images/mail_icon.png" alt="Mail" /> hello@plivo.com</p>
+          </li>
+          <li class="divider-vertical"></li>
+          <li>
+            <div class="btn-group top_menu">
+              <a class="btn login" href="https://manage.plivo.com/accounts/login/">Account</a>
+              <a class="btn btn-info signup" href="https://manage.plivo.com/accounts/register/">Sign Up</a>
+            </div>
+          </li>
+          
+                </ul>
+              </div><!--/.nav-collapse -->
+            </div>
+          </div>
+        </div>
+
+    <div class="main">
+      
+      
+      
+
+      
+<div class="row-fluid home_background">
+  <div class="span12 home_features">
+    <h2>Cloud Platform to Build Voice & SMS Applications</h2>
+    <h4>Build powerful Telephony Apps. Leave the Infrastructure plumbing to Us.</h4>
+    <div class="span2 fake_col"></div>
+    <div class="span2 hover">
+      <a href="/voice/">
+      <div class="phone">
+    <img src="https://plivo-static.s3.amazonaws.com/images/phone_1.png" alt="Voice Call" class="img" />
+    <img src="https://plivo-static.s3.amazonaws.com/images/phone_1_h.png" class="img_hover" alt="Call" />
+      </div>
+      <h3>Voice Calls</h3>
+      <p><strong>Make and receive calls,</strong> conferences, voice broadcasting, real-time call control and more</p>
+      </a>
+    </div>
+    <div class="span2 hover">
+      <a href="/sms/">
+      <div class="chat">
+    <img src="https://plivo-static.s3.amazonaws.com/images/chat_1.png" alt="SMS" class="img" />
+    <img src="https://plivo-static.s3.amazonaws.com/images/chat_1_h.png" class="img_hover" alt="Chat" />
+      </div>
+      <h3>SMS</h3>
+      <p><strong>Build two-way SMS messaging applications</strong> including group messaging with our simple APIs</p>
+      </a>
+    </div>
+    <div class="span2 hover">
+      <a href="/sip/">
+      <div class="sip">
+    <img src="https://plivo-static.s3.amazonaws.com/images/sip_1.png" alt="SIP" class="img" />
+    <img src="https://plivo-static.s3.amazonaws.com/images/sip_1_h.png" class="img_hover" alt="SIP" />
+      </div>
+      <h3>SIP</h3>
+      <p><strong>Native SIP support</strong> (inbound, outbound, registration) enabling seamless integration for businesses</p>
+      </a>
+    </div>
+    <div class="span2 hover">
+      <a href="/sdk/">
+      <div class="webrtc new_holder">
+    <img src="https://plivo-static.s3.amazonaws.com/images/flow_1.png" alt="PlivoSDK" class="img" />
+    <div class="new"></div>
+    <img src="https://plivo-static.s3.amazonaws.com/images/flow_1_h.png" class="img_hover" alt="WebRTC" />
+      </div>
+      <h3>PlivoSDK</h3>
+      <p><strong>Real time communication from your browser & mobiles</strong> anytime, anywhere. No plugins needed</p>
+      </a>
+    </div>
+  </div>
+</div>
+<div class="row-fluid home_background center">
+  <a href="https://manage.plivo.com/accounts/register/" class="btn btn-info btn-largeBtn signup space">Sign Up</a>
+</div>
+<div class="row-fluid center gray_bg">
+  <a href="http://blog.plivo.com/post/45362689649/free-plivo-developer-accounts-for-life"><img src="https://plivo-static.s3.amazonaws.com/images/ful_i.png" alt="" class="announcement_icon"> Plivo now offers free developer accounts for life! <span>Read more</span></a>
+</div>
+<div class="row-fluid center">
+  <h3 class="casestudy_header">Why businesses trust us!</h3>
+</div>
+<div class="shadow_line"></div>
+<div class="row-fluid casestudy_summary">
+  <div class="span12">
+    <div class="span4 offset2">
+      <div class="span4">
+    <a href="/casestudy/calliflower/">
+      <img src="https://plivo-static.s3.amazonaws.com/images/customer_calliflower.png" alt="Calliflower Logo" />
+    </a>
+      </div>
+      <div class="span8">
+    <h4>Powerful and Open Platform</h4>
+    <p>Calliflower revolutionizes the conference experience and maintains their carrier relationships.</p>
+    <p><a href="/casestudy/calliflower/" class="more">Read more</a></p>
+      </div>
+    </div>
+    <div class="span4">
+      <div class="span4">
+    <a href="/casestudy/imprezzio/">
+    <img src="https://plivo-static.s3.amazonaws.com/images/customer_imprezzio.png" alt="Imprezzio Logo" />
+    </a>
+      </div>
+      <div class="span8">
+    <h4>Quality and 24/7 Support</h4>
+    <p>Imprezzio provides their fortune 100 customers with a high quality and secure solution to manage client relationships.</p>
+    <p><a href="/casestudy/imprezzio/" class="more">Read more</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row-fluid casestudy_summary">
+  <div class="span12">
+    <div class="span4 offset2">
+      <div class="span4">
+    <a href="/casestudy/calltacular/">
+      <img src="https://plivo-static.s3.amazonaws.com/images/customer_calltacular.png" alt="Calltacular Logo" />
+    </a>
+      </div>
+      <div class="span8">
+    <h4>Volume pricing from the start</h4>
+    <p>Calltacular scales its business and benefits financially in the process.</p>
+    <p><a href="/casestudy/calltacular/" class="more">Read more</a></p>
+      </div>
+    </div>
+    <div class="span4">
+      <div class="span4">
+    <a href="/casestudy/meutaro/">
+      <img src="https://plivo-static.s3.amazonaws.com/images/customer_MeuTaro.png" alt="MeuTaro Logo" />
+    </a>
+      </div>
+      <div class="span8">
+    <h4>International coverage</h4>
+    <p>MeuTarô, an astrological marketplace finds its fortune by simplifying and automating communications.</p>
+    <p><a href="/casestudy/meutaro/" class="more">Read more</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
+
+    <div class="footer">
+      <div class="row-fluid">
+    <div class="span12">
+            <div class="span2 offset2 contact">
+        <div class="block"></div>
+        <ul>
+          <li><img src="https://plivo-static.s3.amazonaws.com/images/twitter.png" alt="Twitter"><a href="https://twitter.com/plivo">@plivo</a></li>
+          <li><img src="https://plivo-static.s3.amazonaws.com/images/phone.png" alt="Call"><a href="tel:+1-855-46-75486">+1-855-46-75486</a></li>
+          <li><img src="https://plivo-static.s3.amazonaws.com/images/mail.png" alt="Mail"><a href="mailto:hello@plivo.com">hello@plivo.com</a></li>
+          <li><p class="copyright">&copy; 2011-2013 Plivo Inc.</p></li>
+        </ul>
+            </div>
+      <div class="span5 offset1">
+              <div class="span4">
+          <div>
+        <h3>Company</h3>
+        <ul>
+          <li><a href="/about/">About</a></li>
+            <li><a href="http://blog.plivo.com/">Blog</a></li>
+          <li><a href="/terms/">Terms</a></li>
+          <li><a href="/privacy/">Privacy</a></li>
+          <li><a href="/support/">Support</a></li>
+        </ul>
+          </div>
+        </div>
+        <div class="span4">
+          <h3>Developers</h3>
+          <ul>
+        <li><a href="/docs/">Documentation</a></li>
+        <li><a href="/docs/helpers/">Helpers</a></li>
+        <li><a href="/open-source/">Open Source</a></li>
+          </ul>
+        </div>
+        <div class="span4">
+          <h3>Navigation</h3>
+          <ul>
+        <li><a href="/pricing/">Pricing</a></li>
+        <li><a href="https://manage.plivo.com/accounts/register/">Sign Up</a></li>
+        <li><a href="https://manage.plivo.com/accounts/login/">Log In</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+      </div>
+    </div>
+
+    <script src="https://plivo-static.s3.amazonaws.com/js/jquery.min.js"></script>
+    <script src="https://plivo-static.s3.amazonaws.com/js/bootstrap.min.js"></script>
+    
+<script type="text/javascript">
+  $(function() {
+    $(".hover").hover(function() {
+      $(this).find(".img").hide();
+      $(this).find(".img_hover").show();
+    }, function() {
+      $(this).find(".img").show();
+      $(this).find(".img_hover").hide();
+    });
+    $(".gray_bg a").css("opacity", 0);
+    $(".gray_bg a").animate({"opacity": 1}, 2000);
+  });
+</script>
+
+    
+<script type="text/javascript">/*{literal}<![CDATA[*/window.olark||(function(c){var f=window,d=document,l=f.location.protocol=="https:"?"https:":"http:",z=c.name,r="load";var nt=function(){f[z]=function(){(a.s=a.s||[]).push(arguments)};var a=f[z]._={},q=c.methods.length;while(q--){(function(n){f[z][n]=function(){f[z]("call",n,arguments)}})(c.methods[q])}a.l=c.loader;a.i=nt;a.p={0:+new Date};a.P=function(u){a.p[u]=new Date-a.p[0]};function s(){a.P(r);f[z](r)}f.addEventListener?f.addEventListener(r,s,false):f.attachEvent("on"+r,s);var ld=function(){function p(hd){hd="head";return["<",hd,"></",hd,"><",i,' onl' + 'oad="var d=',g,";d.getElementsByTagName('head')[0].",j,"(d.",h,"('script')).",k,"='",l,"//",a.l,"'",'"',"></",i,">"].join("")}var i="body",m=d[i];if(!m){return setTimeout(ld,100)}a.P(1);var j="appendChild",h="createElement",k="src",n=d[h]("div"),v=n[j](d[h](z)),b=d[h]("iframe"),g="document",e="domain",o;n.style.display="none";m.insertBefore(n,m.firstChild).id=z;b.frameBorder="0";b.id=z+"-loader";if(/MSIE[ ]+6/.test(navigator.userAgent)){b.src="javascript:false"}b.allowTransparency="true";v[j](b);try{b.contentWindow[g].open()}catch(w){c[e]=d[e];o="javascript:var d="+g+".open();d.domain='"+d.domain+"';";b[k]=o+"void(0);"}try{var t=b.contentWindow[g];t.write(p());t.close()}catch(x){b[k]=o+'d.write("'+p().replace(/"/g,String.fromCharCode(92)+'"')+'");d.close();'}a.P(2)};ld()};nt()})({loader: "static.olark.com/jsclient/loader0.js",name:"olark",methods:["configure","extend","declare","identify"]});
+  /* custom configuration goes here (www.olark.com/documentation) */
+  olark.identify('7936-763-10-3472');/*]]>{/literal}*/
+  olark('api.chat.updateVisitorNickname', {
+  snippet:  "Anonymous"
+  });
+</script>
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+
+        
+
+ <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-23474761-2']);
+  _gaq.push(['_setDomainName', 'plivo.com']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>         
+        
+        
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 997380994;
+var google_conversion_label = "ZuyQCKag6gQQgqfL2wM";
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/997380994/?value=0&amp;label=ZuyQCKag6gQQgqfL2wM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>    
+        
+        
+  </body>
+</html>
